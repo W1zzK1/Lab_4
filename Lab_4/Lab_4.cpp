09d4e4659f125e3c45f4edc8cff7034d76d253ff
W1zzA <W1zzA@W1ZZK1>
2022-09-19 20:12:52 +0300

    Добавьте файлы проекта.

---
+++ Lab_4/Lab_4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cin >> a >> b >> c;
+	if ((a < b) && (a < c)) {
+		cout << "Наименьшее число : " << a;
+	}
+	else if ((b < a) && (b < c)) {
+		cout << "Наименьшее число : " << b;
+	}
+	else if ((c < a) && (c < b)) {
+		cout << "Наименьшее число : " << c;
+	}
+	else {
+		cout << "Нет наименьшего числа";
+	}
+	return 0;
+}
